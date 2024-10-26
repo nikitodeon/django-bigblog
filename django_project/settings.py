50416e0dee27c422bcc453ecff59fe3e0f02975d
Nikita <ragerage333@gmail.com>
2024-10-26 12:34:14 +0300

    new local settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -145,3 +145,8 @@ EMAIL_USE_TLS=True
 EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASS')
 #DEFAULT_FROM_EMAIL=os.environ.get('EMAIL_USER')
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production ")
